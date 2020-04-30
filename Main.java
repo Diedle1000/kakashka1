b13dabadc7005aa9490cda6016665e78d04c7d25
Diedle1000 <60897095+Diedle1000@users.noreply.github.com>
2020-04-30 17:55:11 +0300

    first commit

---
+++ Main.java
@@ -0,0 +1,18 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+	int z;
+	Scanner in = new Scanner(System.in);
+	int a = in.nextInt();
+	int b = in.nextInt();
+	int c = in.nextInt();
+	in.close();
+	z=((a-3)*b/2)+c;
+        System.out.println("Your number =" + z);
+
+    }
+}
